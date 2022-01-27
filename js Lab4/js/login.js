ed0052b27359b86de6909725d69f93256a4f8107
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-01-27 21:23:35 +0200

    Finish task 1 lab4

--- js Lab4/js/login.js
+++ js Lab4/js/login.js
@@ -1,3 +1,18 @@
-document.ready(function () {
+var btnLogin = document.getElementById("BtnLogin");
+var txtUserName = document.getElementById("TxtUser");
+var txtPasswordName = document.getElementById("TxtPassword");
+var username = "admin";
+var password = "123";
+var welcomeText = document.getElementById("WelcomeText");
+var wrongCredentialsText = document.getElementById("WrongCredentialText");
 
-});
+
+function btnLoginClicked(event) {
+    if (txtUserName.value === "admin" && txtPasswordName.value === "123") {
+        welcomeText.style.display = "block";
+        wrongCredentialsText.style.display = "none";
+    } else {
+        welcomeText.style.display = "none";
+        wrongCredentialsText.style.display = "block";
+    }
+}
