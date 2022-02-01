e3eb50412e079b93edbbe1843c36528f085aab75
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-02-01 15:03:10 +0200

    Enhance  parent and children selectors

--- js Lab4/ToDoList/js/toDoList.js
+++ js Lab4/ToDoList/js/toDoList.js
@@ -1,14 +1,12 @@
 var btnAddTask = document.getElementById("btnAddTask");
 var txtTaskName = document.getElementById("txtTaskName");
-// var btnsDone = document.getElementsByName("btnDone");
-// var btnRemove = document.getElementsByName("btnRemove");
 
 var tasksSection = document.getElementById("Tasks");
 
 function btnAddTaskClick() {
     var newTaskElement = `
         <div class="task">
-            <div class="task-body">` + txtTaskName.value + `</div>
+            <div id="x" class="task-body">` + txtTaskName.value + `</div>
             <div class="task-controls">
                 <a class="btn-done" onclick="btnDoneClicked(event)"><i class="fas fa-check"></i></a>
                 <a class="btn-remove" onclick="btnDeleteClicked(event)"><i class="fas fa-trash-alt"></i></a>
@@ -20,13 +18,13 @@ function btnAddTaskClick() {
 }
 
 function btnDoneClicked(event) {
-    var task = event.currentTarget.parentElement.previousElementSibling;
+    // console.log(event.currentTarget.closest('.task-name')
+    var task = event.currentTarget.closest('.task').firstElementChild;
     task.classList.toggle("finished-task");
-
 }
 
 function btnDeleteClicked(event) {
-    var task = event.currentTarget.parentElement.parentElement;
+    var task = event.currentTarget.closest('.task');
     tasksSection.removeChild(task);
     event.stopPropagation();
 }
