889b5692bfcef8dd246d54c0d28ae0a535c0b5ce
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-02-02 19:39:49 +0200

    Complete Alarm task

--- js Lab5/Alarm/js/Alarm.js
+++ js Lab5/Alarm/js/Alarm.js
@@ -1,25 +1,133 @@
 // Get the modal
 var modal = document.getElementById("setAlarmModal");
 
-// Get the button that opens the modal
 var btn = document.getElementById("btnOpenSetAlarmDialog");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
 btn.onclick = function () {
     modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+
+/// Alarm Login
+var hrsSection = document.getElementById("Hrs");
+var minSection = document.getElementById("Min");
+var secSection = document.getElementById("Sec");
+
+// days
+var days = {
+    SAT: document.getElementById("SAT"),
+    SUN: document.getElementById("SUN"),
+    MON: document.getElementById("MON"),
+    TUE: document.getElementById("TUE"),
+    WED: document.getElementById("WED"),
+    THU: document.getElementById("THU"),
+    FRI: document.getElementById("FRI")
+};
+
+// notifications
+var alarmNotification = document.getElementById("AlarmEnabled");
+var amNotification = document.getElementById("Am");
+var pmNotification = document.getElementById("Pm");
+
+var btnSetAlarm = document.getElementById("btnSetAlarm");
+var btnClear = document.getElementById("btnClear");
+var txtHrsInput = document.getElementById("txtHrs");
+var txtMinInput = document.getElementById("txtMin");
+var txtSecInput = document.getElementById("txtSec");
+
+
+setInterval(refreshClock, 1000);
+
+// Events
+btnSetAlarm.addEventListener("click", function (event) {
+    var hrsInput = Number(txtHrsInput.value);
+    var minInput = Number(txtMinInput.value);
+    var secInput = Number(txtSecInput.value);
+    var today = new Date();
+    var hrsRemaining = (today.getHours() + hrsInput);
+    var minRemaining = (today.getMinutes() + minInput);
+    var secRemaining = (today.getSeconds() + secInput);
+
+    hrsSection.innerText = today.getHours();
+    minSection.innerText = today.getMinutes();
+    secSection.innerText = today.getSeconds();
+
+    var targetDate = new Date(hrsRemaining + minRemaining + secRemaining);
+
+    console.log(targetDate);
+
+    if (hrsRemaining > 12) {
+        pmNotification.classList.add("notification-selected");
+        amNotification.classList.remove("notification-selected");
+    } else {
+        pmNotification.classList.remove("notification-selected");
+        amNotification.classList.add("notification-selected");
+    }
+
+    console.log(alarmNotification);
+    alarmNotification.classList.add("notification-selected");
+
+    modal.style.display = "none";
+
+    var milliSeconds = targetDate.getTime() * 100;
+    console.log(milliSeconds);
+
+    setTimeout(function () {
+        alert("wake up.....");
+    }, milliSeconds);
+
+});
+
+btnClear.addEventListener("click", function (event) {
+    txtHrsInput.value = 0;
+    txtMinInput.value = 0;
+    txtSecInput.value = 0;
+});
+
+function refreshClock() {
+    var today = new Date();
+    var day = today.getDay();
+    hrsSection.innerText = today.getHours();
+    minSection.innerText = today.getMinutes();
+    secSection.innerText = today.getSeconds();
+
+    switch (day) {
+        case 0 :
+            days.SAT.classList.toggle("day-selected");
+            break;
+        case 1 :
+            days.SUN.classList.toggle("day-selected");
+            break;
+        case 2 :
+            days.MON.classList.toggle("day-selected");
+            break;
+        case 3 :
+            days.TUE.classList.toggle("day-selected");
+            break;
+        case 4 :
+            days.WED.classList.toggle("day-selected");
+            break;
+        case 5 :
+            days.THU.classList.toggle("day-selected");
+            break;
+        case 6 :
+            days.FRI.classList.toggle("day-selected");
+            break;
+
+
+    }
+
+}
+
+
+
